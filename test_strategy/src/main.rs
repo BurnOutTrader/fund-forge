4953305743dadea01c01654fe85a6cb6e6d100e0
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-08-23 12:59:04 +1000

    add unit test

--- test_strategy/src/main.rs
+++ test_strategy/src/main.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use chrono::{Duration, NaiveDate};
 use chrono_tz::Australia;
 use tokio::sync::{mpsc, Notify};
+use tokio::sync::mpsc::Receiver;
 use ff_strategies::fund_forge_strategy::FundForgeStrategy;
 use ff_standard_lib::apis::vendor::DataVendor;
 use ff_standard_lib::server_connections::{PlatformMode};
@@ -9,7 +10,7 @@ use ff_standard_lib::standardized_types::base_data::base_data_enum::BaseDataEnum
 use ff_standard_lib::standardized_types::base_data::base_data_type::BaseDataType;
 use ff_standard_lib::standardized_types::base_data::traits::BaseData;
 use ff_standard_lib::standardized_types::enums::{MarketType, Resolution, StrategyMode};
-use ff_standard_lib::standardized_types::subscriptions::DataSubscription;
+use ff_standard_lib::standardized_types::subscriptions::{DataSubscription, DataSubscriptionEvent};
 use ff_strategies::messages::strategy_events::{EventTimeSlice, StrategyEvent, StrategyInteractionMode};
 
 
@@ -31,7 +32,7 @@ fn set_subscriptions_initial() -> Vec<DataSubscription> {
 
 #[tokio::main]
 async fn main() {
-    let (strategy_event_sender, strategy_event_receiver) = mpsc::channel(1000000);
+    let (strategy_event_sender, strategy_event_receiver) = mpsc::channel(1000);
     let notify = Arc::new(Notify::new());
     // we initialize our strategy as a new strategy, meaning we are not loading drawing tools or existing data from previous runs.
     let strategy = FundForgeStrategy::initialize(
@@ -92,10 +93,10 @@ pub async fn on_data_received(strategy: FundForgeStrategy, notify: Arc<Notify>, 
                                     continue
                                 }*/
                                 if candle.is_closed {
-                                    println!("{}...Candle {}, {}: close:{} at {}, is_closed: {}", strategy.time_utc().await, candle.resolution, candle.symbol.name, candle.close, base_data.time_created_utc(), candle.is_closed); //note we automatically adjust for daylight savings based on historical daylight savings adjustments.
+                                    //println!("{}...Candle {}, {}: close:{} at {}, is_closed: {}", strategy.time_utc().await, candle.resolution, candle.symbol.name, candle.close, base_data.time_created_utc(), candle.is_closed); //note we automatically adjust for daylight savings based on historical daylight savings adjustments.
                                 } else {
                                     //Todo Documents, Open bars get sent through with every tick, so you can always access the open bar using highest resolution.
-                                    //println!("{}...Open Candle {}: close:{} at {}, is_closed: {}", strategy.time_utc().await, candle.symbol.name, candle.close, base_data.time_created_utc(), candle.is_closed); //note we automatically adjust for daylight savings based on historical daylight savings adjustments.
+                                    //println!("{}...Open Candle {}: close:{} at {}, is_closed: {}", strategy.time_utc().await, candle.symbol.name, candle.close, base_data.time_utc(), candle.is_closed); //note we automatically adjust for daylight savings based on historical daylight savings adjustments.
                                 }
 
                                 //println!("{}... time local {}", count, strategy.time_local().await);
@@ -164,4 +165,114 @@ pub async fn on_data_received(strategy: FundForgeStrategy, notify: Arc<Notify>, 
     }
     event_receiver.close();
     println!("Strategy Event Loop Ended");
+}
+
+
+
+/// Tests for strategy functionality
+fn set_test_subscriptions_initial() -> Vec<DataSubscription> {
+    let subscriptions: Vec<DataSubscription> = vec![
+        DataSubscription::new("AUD-CAD".to_string(), DataVendor::Test, Resolution::Ticks(1), BaseDataType::Ticks, MarketType::Forex),
+        DataSubscription::new("AUD-USD".to_string(), DataVendor::Test, Resolution::Ticks(1), BaseDataType::Ticks, MarketType::Forex),
+        DataSubscription::new("AUD-CAD".to_string(), DataVendor::Test, Resolution::Minutes(15), BaseDataType::Candles, MarketType::Forex)
+    ];
+    subscriptions
+}
+
+async fn initialize_test_strategy() -> (Receiver<EventTimeSlice>, FundForgeStrategy, Arc<Notify>) {
+    let (strategy_event_sender, strategy_event_receiver) = mpsc::channel(100);
+    let notify = Arc::new(Notify::new());
+    // we initialize our strategy as a new strategy, meaning we are not loading drawing tools or existing data from previous runs.
+    let strategy = FundForgeStrategy::initialize(
+        Some(String::from("test")), //if none is passed in an id will be generated based on the executing program name, todo! this needs to be upgraded in the future to be more reliable in Single and Multi machine modes.
+        notify.clone(),
+        PlatformMode::SingleMachine, // SingleMachine or MultiMachine, multi machine mode for co-located servers, single machine for and MT5/Ninjatrader style platform
+        StrategyMode::Backtest, // Backtest, Live, LivePaper
+        StrategyInteractionMode::SemiAutomated,  // In semi-automated the strategy can interact with the user drawing tools and the user can change data subscriptions, in automated they cannot. // the base currency of the strategy
+        NaiveDate::from_ymd_opt(2023, 03, 20).unwrap().and_hms_opt(0, 0, 0).unwrap(), // Starting date of the backtest is a NaiveDateTime not NaiveDate
+        NaiveDate::from_ymd_opt(2023, 03, 22).unwrap().and_hms_opt(0, 0, 0).unwrap(), // Ending date of the backtest is a NaiveDateTime not NaiveDate
+        Australia::Sydney, // the strategy time zone
+        Duration::days(1), // the warmup duration, the duration of historical data we will pump through the strategy to warm up indicators etc before the strategy starts executing.
+        set_test_subscriptions_initial(), //the closure or function used to set the subscriptions for the strategy. this allows us to have multiple subscription methods for more complex strategies
+        strategy_event_sender, // the sender for the strategy events
+        None,
+        100
+    ).await;
+    (strategy_event_receiver, strategy, notify)
+}
+#[tokio::test]
+pub async fn test_subscribing_and_consolidating()  {
+    let (mut event_receiver, strategy, notify) = initialize_test_strategy().await;
+    let mut warmup_complete = false;
+    let mut count = 0;
+    let mut subscription_events = 0;
+    'strategy_loop: while let Some(event_slice) = event_receiver.recv().await {
+        if warmup_complete {
+            count += 1;
+            if count == 1000 {
+                let aud_cad_60m = DataSubscription::new("AUD-CAD".to_string(), DataVendor::Test, Resolution::Minutes(60), BaseDataType::Candles, MarketType::Forex);
+                let aud_usd_15m = DataSubscription::new("AUD-USD".to_string(), DataVendor::Test, Resolution::Minutes(15), BaseDataType::Candles, MarketType::Forex);
+                strategy.subscriptions_update(vec![aud_usd_15m.clone(), aud_cad_60m.clone()],100).await;
+            }
+        }
+        for strategy_event in event_slice {
+            match strategy_event {
+                StrategyEvent::TimeSlice(_time, time_slice) => {
+                    'base_data_loop: for base_data in &time_slice {
+                        match base_data {
+                            BaseDataEnum::Candle(ref candle) => {
+                                if candle.is_closed && count < 1000 {
+                                    assert_eq!(candle.resolution, Resolution::Minutes(15) )
+                                }
+                                if candle.is_closed && count > 1000 {
+                                    assert!(matches!(candle.resolution, Resolution::Minutes(15) | Resolution::Minutes(60)));
+                                } 
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                StrategyEvent::DataSubscriptionEvents(_, events, _) => {
+                    for event in events {
+                        subscription_events += 1;
+                            match event {
+                                DataSubscriptionEvent::Subscribed(subscription) => {
+                                    if subscription_events == 1 {
+                                        assert_eq!(subscription.symbol.name, "AUD-USD");
+                                        assert_eq!(subscription.resolution, Resolution::Ticks(1));
+                                    }
+                                    if subscription_events == 2 {
+                                        assert_eq!(subscription.symbol.name, "AUD-CAD");
+                                        assert_eq!(subscription.resolution, Resolution::Ticks(1));
+                                    }
+                                    if subscription_events == 3 {
+                                        assert_eq!(subscription.symbol.name, "AUD-CAD");
+                                        assert_eq!(subscription.resolution, Resolution::Minutes(15));
+                                    }
+                                    if subscription_events == 4 {
+                                        assert_eq!(subscription.symbol.name, "AUD-USD");
+                                        assert_eq!(subscription.resolution, Resolution::Minutes(15));
+                                    }
+                                    if subscription_events == 5 {
+                                        assert_eq!(subscription.symbol.name, "AUD-CAD");
+                                        assert_eq!(subscription.resolution, Resolution::Minutes(60));
+                                    }
+                                }
+                                DataSubscriptionEvent::Unsubscribed(subscription) => {
+                                    assert_eq!(subscription.symbol.name, "AUD-CAD");
+                                    assert_eq!(subscription.resolution, Resolution::Minutes(15));
+                                }
+                            }
+                        }
+                    },
+                    StrategyEvent::ShutdownEvent(_, _) => break 'strategy_loop,
+                    StrategyEvent::WarmUpComplete(_) => {
+                        warmup_complete = true;
+                    }
+                    _ => {}
+            }
+            notify.notify_one();
+        }
+    }
+    event_receiver.close();
 }
