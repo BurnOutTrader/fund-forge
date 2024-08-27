be0e3cfefe64e8327c21f165979446505575872d
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-08-27 11:01:19 +1000

    add example on using AsyncIndicators trait

--- ff_standard_lib/src/indicators/indicators_trait.rs
+++ ff_standard_lib/src/indicators/indicators_trait.rs
@@ -7,13 +7,13 @@ use crate::standardized_types::subscriptions::DataSubscription;
 pub type IndicatorLongName = String;
 pub type IndicatorName = String;
 
-
 #[async_trait]
 pub trait AsyncIndicators: Indicators {
     /// Updates the indicator with the new data point.
-    /// be aware you need to make update_base_data().await and async fn and use a tokio::Mutex to lock to the type when locking, since this type depends upon interior mutability
-    /// without a lock we have a potential race condition where the timeslice contains multiple data points for a subscription,
-    /// to cheaply resolve this we simply use a mutex lock inside our type to prevent multple calls to update being ran at same moment
+    /// be aware you need to make update_base_data().await an async fn and use a tokio::Mutex to lock the type when updating, since this type depends upon interior mutability
+    /// without a lock we have a potential for a race condition where the time_slice contains multiple data points for a subscription,
+    /// to cheaply resolve this we simply use a mutex lock inside our type to prevent multiple calls to update running in the same moment.
+    /// this allows the Handler to pass the data without slowing down its own fn, but also allows it updates to proceed concurrently even when a high volume of data is being processed.
     ///
     /// ### Example
     /// ```rust
@@ -29,46 +29,28 @@ pub trait AsyncIndicators: Indicators {
     /// pub struct AverageTrueRange {
     ///     name: IndicatorName,
     ///     subscription: DataSubscription,
-    ///    history: RollingWindow<IndicatorValues>,
+    ///     history: RollingWindow<IndicatorValues>,
     ///     base_data_history: RollingWindow<BaseDataEnum>,
     ///     is_ready: bool,
     ///     period: u64,
     ///     tick_size: f64,
+    /// 
+    ///     /// See below example on why we need to use this lock update_base_data().await and async fn
     ///     lock: tokio::sync::Mutex<()>
     /// }
     ///
+    /// impl Indicators for AverageTrueRange { 
+    ///     // complete implementation here
+    /// }
+    /// 
     /// #[async_trait]
     /// impl AsyncIndicators for AverageTrueRange {
+    ///     //To protect against race conditions where a time slice contains multiple data points of same subscription when using the IndicatorHandler
     ///     async fn update_base_data(&mut self, base_data: &BaseDataEnum) -> Option<IndicatorValues> {
-    ///         let _lock = self.lock.lock().await; //todo to protect against race conditions where a time slice contains multiple data points of same subscription when using the IndicatorHandler
-    ///         if !base_data.is_closed() {
-    ///             return None
-    ///         }
-    ///         
-    ///         self.base_data_history.add(base_data.clone());
-    ///         if self.is_ready == false {
-    ///             if !self.base_data_history.is_full() {
-    ///                 return None
-    ///             } else {
-    ///                 self.is_ready = true;
-    ///             }
-    ///         }
-    ///
-    ///         let atr = self.calculate_true_range();
-    ///         if atr == 0.0 {
-    ///             return None
-    ///         }
-    ///
-    ///         let mut plots = AHashMap::new();
-    ///         plots.insert("atr".to_string(), atr);
-    ///         let result = IndicatorValues::new(self.name(), self.subscription(), plots, base_data.time_created_utc());
-    ///         self.history.add(result.clone());
-    ///
-    ///         Some(result)
+    ///         let _lock = self.lock.lock().await; 
+    ///         //update the indicator here
     ///     }
     /// }
-    ///
-    /// impl Indicators for AverageTrueRange { // complete implementation here}
     /// ```
     async fn update_base_data(&mut self, base_data: &BaseDataEnum) -> Option<IndicatorValues>;
 }
