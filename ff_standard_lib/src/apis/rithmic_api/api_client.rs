135126670ceaeb2a5f119114355a96a255f771af
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-09-19 12:27:33 +1000

    add callback manager to api

--- ff_standard_lib/src/apis/rithmic_api/api_client.rs
+++ ff_standard_lib/src/apis/rithmic_api/api_client.rs
@@ -7,6 +7,10 @@ use ff_rithmic_api::credentials::RithmicCredentials;
 use ff_rithmic_api::rithmic_proto_objects::rti::request_login::SysInfraType;
 use lazy_static::lazy_static;
 use prost::Message as ProstMessage;
+use tokio::io::WriteHalf;
+use tokio::net::TcpStream;
+use tokio::sync::Mutex;
+use tokio_rustls::server::TlsStream;
 use crate::apis::brokerage::broker_enum::Brokerage;
 use crate::apis::brokerage::server_side_brokerage::BrokerApiResponse;
 use crate::apis::data_vendor::datavendor_enum::DataVendor;
@@ -39,9 +43,28 @@ pub struct RithmicClient {
     /// The primary client is the only client used for data feeds, it will also have all brokerage features.
     pub client: Arc<RithmicApiClient>,
     pub symbol_info: DashMap<SymbolName, SymbolInfo>,
+    pub callbacks: DashMap<String, DashMap<u64, Arc<Mutex<WriteHalf<TlsStream<TcpStream>>>>>>
 }
 
 impl RithmicClient {
+    pub fn add_callback(&self, stream_name: String, id: u64, client_ref: Arc<Mutex<WriteHalf<TlsStream<TcpStream>>>>) {
+        if let Some(map) = self.callbacks.get(&stream_name) {
+            map.insert(id, client_ref);
+        } else {
+            let mut map = DashMap::new();
+            map.insert(id, client_ref);
+            self.callbacks.insert(stream_name.clone(), map);
+        }
+    }
+
+    pub fn get_callback_client(&self, stream_name: &str, id: u64) -> Option<Arc<Mutex<WriteHalf<TlsStream<TcpStream>>>>> {
+        if let Some(map) = self.callbacks.get(stream_name) {
+            if let Some((_, client)) = map.value().remove(&id) {
+                return Some(client)
+            }
+        }
+        None
+    }
     fn rithmic_credentials(broker: &Brokerage) -> RithmicCredentials {
         let file = format!("rithmic_credentials_{}.toml", broker.to_string().to_lowercase());
         let file_path = format!("rithmic_credentials/{}", file);
@@ -53,6 +76,7 @@ impl RithmicClient {
         let client = Self {
             client: Arc::new(client),
             symbol_info: Default::default(),
+            callbacks: Default::default(),
         };
         let shutdown_client = || async {
             if let Err(_) = block_on(client.client.shutdown_all()) {}
@@ -96,53 +120,58 @@ impl RithmicClient {
     }
 
     // Send a request and wait for a response
-    pub async fn send_synchronous_request<T: ProstMessage + Default>(&self, plant: &SysInfraType, request: T, id: u64) -> Result<DataServerResponse, FundForgeError> {
-        todo!()
+    pub async fn send_synchronous_request<T: ProstMessage + Default>(&self, stream_name: String, plant: &SysInfraType, request: T, id: u64 ) -> Result<DataServerResponse, FundForgeError> {
+       todo!()
     }
 }
 
 #[async_trait]
 impl BrokerApiResponse for RithmicClient {
-    async fn symbols_response(&self, market_type: MarketType, callback_id: u64) -> DataServerResponse {
+    async fn symbols_response(&self, stream_name: String, market_type: MarketType, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn account_info_response(&self, account_id: AccountId, callback_id: u64) -> DataServerResponse {
+    async fn account_info_response(&self, stream_name: String, account_id: AccountId, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn symbol_info_response(&self, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
+    async fn symbol_info_response(&self, stream_name: String, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn margin_required_historical_response(&self, symbol_name: SymbolName, quantity: Volume, callback_id: u64) -> DataServerResponse {
+    async fn margin_required_historical_response(&self, stream_name: String, symbol_name: SymbolName, quantity: Volume, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn margin_required_live_response(&self, symbol_name: SymbolName, quantity: Volume, callback_id: u64) -> DataServerResponse {
+    async fn margin_required_live_response(&self, stream_name: String, symbol_name: SymbolName, quantity: Volume, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 }
 
 #[async_trait]
 impl VendorApiResponse for RithmicClient {
-    async fn symbols_response(&self, market_type: MarketType, callback_id: u64) -> DataServerResponse{
+    async fn symbols_response(&self, stream_name: String, market_type: MarketType, callback_id: u64) -> DataServerResponse{
+        //1. create a oneshot
+        //2. create the rithmic message
+        //3. send to rithmic
+        //4. await on oneshot
+        //5. process rithmic message here, don't parse it until here, so that each response type can be used for diff functions
         todo!()
     }
 
-    async fn resolutions_response(&self, market_type: MarketType, callback_id: u64) -> DataServerResponse {
+    async fn resolutions_response(&self, stream_name: String, market_type: MarketType, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn markets_response(&self, callback_id: u64) -> DataServerResponse {
+    async fn markets_response(&self, stream_name: String, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn decimal_accuracy_response(&self, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
+    async fn decimal_accuracy_response(&self, stream_name: String, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 
-    async fn tick_size_response(&self, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
+    async fn tick_size_response(&self, stream_name: String, symbol_name: SymbolName, callback_id: u64) -> DataServerResponse {
         todo!()
     }
 }
