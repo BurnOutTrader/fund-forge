c2f194894b669a47ec7fa6d5bb88f888a0e66537
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-09-24 14:44:38 +1000

    make buffer always in milliseconds

--- test_strategy/src/main.rs
+++ test_strategy/src/main.rs
@@ -66,7 +66,7 @@ async fn main() {
         None,
         //strategy resolution, all data at a lower resolution will be consolidated to this resolution, if using tick data, you will want to set this at 1 second or less depending on the data granularity
         //this allows us full control over how the strategy buffers data and how it processes data, in live trading .
-        50,
+        100,
         GUI_DISABLED
     ).await;
 
