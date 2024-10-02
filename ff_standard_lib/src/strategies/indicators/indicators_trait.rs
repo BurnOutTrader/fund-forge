4de941edf7452dce4957340f86da9efde9cf6cf5
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-10-02 21:03:50 +1000

    improve trait docs

--- ff_standard_lib/src/strategies/indicators/indicators_trait.rs
+++ ff_standard_lib/src/strategies/indicators/indicators_trait.rs
@@ -35,7 +35,17 @@ pub trait Indicators {
             }
         }
     }
+    /// The number of Indicator data points 'IndicatorValues` to retain in history, this is passed in on creating a new indicator
     fn history_to_retain(&self) -> usize;
+
+    /// This is where the engine sends data
+    /// The engine will send open and closed bars so you might want to check  if the base_data.is_closed()
+    /// depending on your indicators intentions and logic, open bars will make logic more complex,
+    /// Using a BtreeMap<DateTime, BaseDataEnum> where the key time is the open time of the data is a good way to ensure you only have 1 open bar in your history.
+    /// Since the open time of a bar never changes, each time the same open bar updates it will replace the last version of itself in the map.
+    /// if !base_data.is_closed() {
+    ///     return None;
+    ///  }
     fn update_base_data(&mut self, base_data: &BaseDataEnum) -> Option<IndicatorValues>;
 
     /// Returns the subscription for the indicator.
@@ -47,7 +57,7 @@ pub trait Indicators {
     /// Returns the indicator results at the given index.
     fn index(&self, index: usize) -> Option<IndicatorValues>;
 
-    /// returns the crrent value, useful for update on tick or price change indicators.
+    /// returns the current value, useful for update on tick or price change indicators.
     fn current(&self) -> Option<IndicatorValues>;
 
     fn plots(&self) -> RollingWindow<IndicatorValues>;
@@ -55,6 +65,9 @@ pub trait Indicators {
     /// Returns true if the indicator is ready.
     fn is_ready(&self) -> bool;
 
+    /// Returns the indicators history, we can still use another data structure to store history like a BTreeMap,
+    /// this function would be expensive to use and doesn't server much purpose
+    /// since we can just use strategy.indicator_index(u64) to get the historical data.
     fn history(&self) -> RollingWindow<IndicatorValues>;
 
     /// the number of base data points we need to fill the history on warm up, for example an 5 period ATR indicator that keeps a history of 12 data points will require 17 base data enums to warm up
