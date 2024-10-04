cfb720e945e8b998f44b04aa03957817c4e069c3
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-10-04 10:27:33 +1000

    update historical time handling

--- ff_tests/ledger_test/src/main.rs
+++ ff_tests/ledger_test/src/main.rs
@@ -60,9 +60,9 @@ pub async fn on_data_received(
     let account_1 = AccountId::from("Test_Account_1");
     // The engine will send a buffer of strategy events at the specified buffer interval, it will send an empty buffer if no events were buffered in the period.
     'strategy_loop: while let Some(event_slice) = event_receiver.recv().await {
-        println!("Strategy: Buffer Received Time: {}", strategy.time_local());
+        //println!("Strategy: Buffer Received Time: {}", strategy.time_local());
         for (time, strategy_event) in event_slice.iter() {
-            println!("Strategy: Buffer Event Time: {}", strategy.time_zone().from_utc_datetime(&time.naive_utc()));
+            //println!("Strategy: Buffer Event Time: {}", strategy.time_zone().from_utc_datetime(&time.naive_utc()));
             match strategy_event {
                 StrategyEvent::TimeSlice(time_slice) => {
                     // here we would process the time slice events and update the strategy state accordingly.
