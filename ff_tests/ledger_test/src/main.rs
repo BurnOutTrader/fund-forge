7b6af640adcc2641437919b7f53ec97b58dc4e86
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-10-17 21:34:46 +1100

    change backtest data

--- ff_tests/ledger_test/src/main.rs
+++ ff_tests/ledger_test/src/main.rs
@@ -27,7 +27,7 @@ async fn main() {
         dec!(100000),
         Currency::USD,
         NaiveDate::from_ymd_opt(2024, 6, 5).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-        NaiveDate::from_ymd_opt(2024, 08, 28).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        NaiveDate::from_ymd_opt(2024, 06, 28).unwrap().and_hms_opt(0, 0, 0).unwrap(),
         Australia::Sydney,
         Duration::hours(5),
         vec![
