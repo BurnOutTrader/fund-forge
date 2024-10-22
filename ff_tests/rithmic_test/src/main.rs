ad74137217fb6300475d97897ce40183332954dc
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-10-22 22:30:16 +1100

    minor

--- ff_tests/rithmic_test/src/main.rs
+++ ff_tests/rithmic_test/src/main.rs
@@ -215,8 +215,8 @@ pub async fn on_data_received(
                                     bars_since_entry, open_profit, position_size
                                 );
 
-                                //Add to winners if we have momentum
-                                if (is_long || is_short) && bars_since_entry > 1 && open_profit >= dec!(10) && position_size <= dec!(2) {
+                                //Add to winners up to 2x if we have momentum
+                                if (is_long || is_short) && bars_since_entry > 1 && open_profit >= dec!(10) && position_size <= dec!(5) && add_order_id.is_none() {
                                     let cancel_order_time = Utc::now() + Duration::seconds(5);
                                     if is_long && quotebar.ask_close < last_candle.ask_high {
                                         let new_add_order_id = strategy.stop_limit(&symbol, None, &account, None,dec!(3), OrderSide::Buy,  String::from("Add Long Stop Limit"), last_candle.ask_high + dec!(0.5), last_candle.ask_high + dec!(0.25), TimeInForce::Time(cancel_order_time.timestamp(), UTC.to_string())).await;
@@ -233,17 +233,19 @@ pub async fn on_data_received(
                                 }
 
                                 // Cut losses and take profits, we check entry order is none to prevent exiting while an entry order is unfilled, entry order will expire and go to none on the TIF expiry, or on fill.
-                                if open_profit > dec!(100) || (open_profit < dec!(-30) && bars_since_entry > 10) && add_order_id.is_none() && entry_order_id.is_none() {
+                                if open_profit > dec!(100) || (open_profit < dec!(-30) && bars_since_entry > 10) && add_order_id.is_none() && entry_order_id.is_none() && exit_order_id.is_none() {
                                     let is_long = strategy.is_long(&account, &symbol_code);
                                     let is_short = strategy.is_short(&account, &symbol_code);
-                                    if is_long && exit_order_id == None {
+                                    if is_long
+                                    {
                                         let position_size = strategy.position_size(&account, &symbol_code);
                                         let exit_id = strategy.exit_long(&symbol, None, &account, None, position_size, String::from("Exit Long")).await;
                                         exit_order_id = Some(exit_id);
                                         bars_since_entry = 0;
                                         last_side = LastSide::Long;
                                     }
-                                    else if is_short && exit_order_id == None {
+                                    else if is_short
+                                    {
                                         let position_size = strategy.position_size(&account, &symbol_code);
                                         let exit_id = strategy.exit_short(&symbol, None, &account, None, position_size, String::from("Exit Short")).await;
                                         exit_order_id = Some(exit_id);
@@ -252,18 +254,18 @@ pub async fn on_data_received(
                                     }
                                 }
 
-                                //Take smaller profit if we add and dont get momentum
-                                if bars_since_entry > 5 && open_profit < dec!(60) && open_profit >= dec!(30) && position_size > dec!(2) {
+                                //Take smaller profit if we add and don't get momentum
+                                if bars_since_entry > 5 && open_profit < dec!(60) && open_profit >= dec!(30) && position_size > dec!(2) && add_order_id.is_none() && entry_order_id.is_none() && exit_order_id.is_none() {
                                     let is_long = strategy.is_long(&account, &symbol_code);
                                     let is_short = strategy.is_short(&account, &symbol_code);
-                                    if is_long && exit_order_id == None {
+                                    if is_long {
                                         let position_size = strategy.position_size(&account, &symbol_code);
                                         let exit_id = strategy.exit_long(&symbol, None, &account, None, position_size, String::from("No Momo Exit Long")).await;
                                         exit_order_id = Some(exit_id);
                                         bars_since_entry = 0;
                                         last_side = LastSide::Long;
                                     }
-                                    else if is_short && exit_order_id == None {
+                                    else if is_short {
                                         let position_size = strategy.position_size(&account, &symbol_code);
                                         let exit_id = strategy.exit_short(&symbol, None, &account, None, position_size, String::from("No Momo Exit Short")).await;
                                         exit_order_id = Some(exit_id);
