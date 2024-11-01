1428d09e5b808b30ab51e920c3d89ca9b444366d
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-01 22:52:02 +1100

    minor

--- ff_data_server/src/bitget_api/mod.rs
+++ ff_data_server/src/bitget_api/mod.rs
@@ -1,4 +1,4 @@
 pub mod api_client;
 mod login;
-pub mod vendor_response;
-pub mod broker_response;
+pub mod vendor_api_response;
+pub mod broker_api_response;
--- ff_data_server/src/rithmic_api/mod.rs
+++ ff_data_server/src/rithmic_api/mod.rs
@@ -1,6 +1,6 @@
 pub mod api_client;
 pub mod products;
 pub mod plant_handlers;
-pub mod broker_response;
-pub mod vendor_response;
+pub mod broker_api_response;
+pub mod vendor_api_response;
 pub mod client_base;
