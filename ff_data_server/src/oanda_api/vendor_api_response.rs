8c5cae135773e67e413a80875c8cec05b647a867
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-07 23:38:13 +1100

    minor

--- ff_data_server/src/oanda_api/vendor_api_response.rs
+++ ff_data_server/src/oanda_api/vendor_api_response.rs
@@ -175,6 +175,7 @@ impl VendorApiResponse for OandaClient {
     async fn session_market_hours_response(&self, mode: StrategyMode, stream_name: StreamName, symbol_name: SymbolName, date_time: DateTime<Utc>, callback_id: u64) -> DataServerResponse {
         todo!()
     }
+
     async fn update_historical_data(
         &self,
         symbol: Symbol,
