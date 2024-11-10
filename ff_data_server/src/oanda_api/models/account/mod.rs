f08384405819dfb464682d56331d090fe99c4645
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-11 00:11:48 +1100

    refactoring oanda

--- ff_data_server/src/oanda_api/models/account/mod.rs
+++ ff_data_server/src/oanda_api/models/account/mod.rs
@@ -1,8 +1,8 @@
-pub mod account_change_status;
+
 pub mod account_changes;
 pub mod account_properties;
 pub mod accumulated_account_state;
 pub mod calculated_account_state;
-pub mod enums;
 pub mod guaranteed_stop_loss_parameters;
 pub mod user_attributes;
+pub mod enums;
