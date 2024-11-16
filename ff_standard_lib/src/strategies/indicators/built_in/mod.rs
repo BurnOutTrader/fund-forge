9014121257f226fc81dda3209b20e915dfef4974
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-16 16:35:58 +1100

    add mod

--- ff_standard_lib/src/strategies/indicators/built_in/mod.rs
+++ ff_standard_lib/src/strategies/indicators/built_in/mod.rs
@@ -1,3 +1,4 @@
 pub mod average_true_range;
 pub mod renko;
 pub mod moving_average;
+pub mod exponential_moving_average;
--- ff_standard_lib/src/strategies/indicators/mod.rs
+++ ff_standard_lib/src/strategies/indicators/mod.rs
@@ -3,3 +3,4 @@ pub mod indicators_trait;
 pub mod traits;
 pub mod indicator_events;
 pub mod indicator_values;
+
