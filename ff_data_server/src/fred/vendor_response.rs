90cdd5206ae318da6a7d4f4b1274b9331661c61d
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-25 15:16:44 +1100

    work on fred api

--- ff_data_server/src/fred/vendor_response.rs
+++ ff_data_server/src/fred/vendor_response.rs
@@ -5,14 +5,16 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use chrono::{DateTime, NaiveDate, TimeZone, Utc};
 use chrono_tz::America::Chicago;
+use chrono_tz::Tz;
 use fred_rs::client::FredClient;
 use fred_rs::series::observation::{Builder, Units, Frequency, Response};
-use indicatif::ProgressBar;
+use indicatif::{ProgressBar, ProgressStyle};
 use rust_decimal::Decimal;
 use rust_decimal::prelude::FromPrimitive;
-use tokio::sync::OnceCell;
+use tokio::sync::{OnceCell};
 use ff_standard_lib::messages::data_server_messaging::{DataServerResponse, FundForgeError};
-use ff_standard_lib::product_maps::fred::models::{fred_data_resolutions, FredDataSet};
+use ff_standard_lib::product_maps::fred::models::{fred_release_time_of_day, get_fed_api_country_format, FredDataSetEnum};
+use ff_standard_lib::standardized_types::base_data::base_data_enum::BaseDataEnum;
 use ff_standard_lib::standardized_types::base_data::base_data_type::BaseDataType;
 use ff_standard_lib::standardized_types::base_data::fundamental::Fundamental;
 use ff_standard_lib::standardized_types::datavendor_enum::DataVendor;
@@ -20,6 +22,7 @@ use ff_standard_lib::standardized_types::enums::{MarketType, StrategyMode};
 use ff_standard_lib::standardized_types::resolution::Resolution;
 use ff_standard_lib::standardized_types::subscriptions::{DataSubscription, Symbol, SymbolName};
 use ff_standard_lib::StreamName;
+use crate::server_features::database::hybrid_storage::{DATA_STORAGE};
 use crate::server_features::server_side_datavendor::VendorApiResponse;
 
 static FRED_CLIENT: OnceCell<Arc<FredApiClient>> = OnceCell::const_new();
@@ -108,7 +111,7 @@ impl FredApiClient {
         let mut client = FredClient::new().unwrap();
         client.with_key(&api_key);
         Ok(Self {
-            client
+            client,
         })
     }
 }
@@ -158,95 +161,147 @@ impl VendorApiResponse for FredApiClient {
 
 
     async fn update_historical_data(&self, symbol: Symbol, base_data_type: BaseDataType, resolution: Resolution, from: DateTime<Utc>, to: DateTime<Utc>, from_back: bool, progress_bar: ProgressBar, is_bulk_download: bool) -> Result<(), FundForgeError> {
-        todo!()
-    }
-}
+        const UNITS_ARR: [Units; 9] = [
+            Units::LIN,
+            Units::CHG,
+            Units::CH1,
+            Units::PCH,
+            Units::PC1,
+            Units::PCA,
+            Units::CCH,
+            Units::CCA,
+            Units::LOG,
+        ];
 
-#[tokio::test]
-async fn test_fred_client() {
-    let data_folder = std::path::PathBuf::from("/Volumes/KINGSTON/data");
-    let api_key = parse_fred_api_key(data_folder).unwrap();
-    let mut client = FredClient::new().unwrap();
-    client.with_key(&api_key);
-    // Create the argument builder
+        let fred_data = symbol.name.clone();
+        let symbol = Symbol::new(
+            fred_data.clone(),
+            DataVendor::Fred,
+            MarketType::Fundamentals
+        );
 
+        let length = (to - from).num_days() as u64;
+        progress_bar.set_length(length);
+        progress_bar.set_style(
+            ProgressStyle::default_bar()
+                .template("{prefix:.green} [{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} {msg} ({eta})")
+                .unwrap()
+                .progress_chars("=>-")
+        );
+        progress_bar.set_message(format!("Starting Download for ({}: {}) from: {}, to {}: Message will not change if no data found", resolution, base_data_type, from, to));
+        let increment = match resolution {
+            Resolution::Day => 1,
+            Resolution::Week => 7,
+            Resolution::Month => 30,
+            Resolution::Quarter => 90,
+            Resolution::Year => 365,
+            _ => 1,
+        };
 
-    const UNITS_ARR: [Units; 9] = [
-        Units::LIN,
-        Units::CHG,
-        Units::CH1,
-        Units::PCH,
-        Units::PC1,
-        Units::PCA,
-        Units::CCH,
-        Units::CCA,
-        Units::LOG,
-    ];
-    let fred_data = FredDataSet::GrossNationalProductPerCapita;
-    let resolutions = fred_data_resolutions(fred_data);
-
-    let symbol = Symbol::new(
-        fred_data.to_string(),
-        DataVendor::Fred,
-        MarketType::Fundamentals
-    );
-
-    for resolution in resolutions {
+        let database = DATA_STORAGE.get().unwrap();
         let mut data_map = BTreeMap::new();
 
+        let (symbol_name, country_code) = match symbol.name.split('-').collect::<Vec<&str>>().as_slice() {
+            [symbol_name, country_code] => (symbol_name.to_string(), country_code.to_string()),
+            _ => {
+                return Err(FundForgeError::ClientSideErrorDebug("Invalid symbol format, symbol name should be formatted GDP-USA".to_string()));
+            }
+        };
+
+        let country_name = match get_fed_api_country_format(&country_code) {
+            Some(country_code) => country_code,
+            None => {
+                return Err(FundForgeError::ClientSideErrorDebug("Invalid country code".to_string()));
+            }
+        };
+
+        let fred_data_set = FredDataSetEnum::from_str(&fred_data).unwrap();
+
+        let from_chicago_time = Chicago.from_utc_datetime(&from.naive_utc());
+        let to_chicago_time = Chicago.from_utc_datetime(&to.naive_utc());
+        let (release_hour, release_minutes) = fred_release_time_of_day(fred_data_set);
+
         for unit in UNITS_ARR.iter() {
-            let mut builder = Builder::new();
-            // Set the arguments for the builder
-            builder
-                .observation_start("1930-01-01")
-                .units(unit.clone())
-                .frequency(resolution_to_frequency(resolution).unwrap());
+            let mut last_date: DateTime<Tz> = from_chicago_time;
+           while last_date < to_chicago_time {
+               let from_string = last_date.format("%Y-%m-%d").to_string();
+
+               // Set the arguments for the builder
+               let mut builder = Builder::new();
+               builder
+                   .observation_start(&from_string)
+                   .units(unit.clone())
+                   .frequency(resolution_to_frequency(resolution).unwrap());
 
 
-            // Make the request and pass in the builder to apply the arguments
-            let resp: Response = match client.series_observation(&fred_data.to_string(), Some(builder)) {
-                Ok(resp) => resp,
-                Err(msg) => {
-                    println!("{}", msg);
-                    return
-                },
-            };
+               // Make the request and pass in the builder to apply the arguments
+               let resp: Response = match self.client.series_observation(&fred_data, Some(builder)) {
+                   Ok(resp) => resp,
+                   Err(msg) => {
+                       println!("{}", msg);
+                       continue;
+                   },
+               };
 
-            // Print the response
-            for data in resp.observations {
-                let time = NaiveDate::parse_from_str(&data.date, "%Y-%m-%d")
-                    .expect("Invalid date format")
-                    .and_hms_opt(7, 30, 0).unwrap();
+               if resp.observations.is_empty() {
+                   if last_date >= to || last_date >= from {
+                       break;
+                   }
+                   continue;
+               }
 
-                let ct_time = Chicago.from_local_datetime(&time)
-                    .earliest().unwrap();
+               progress_bar.set_message(format!("Downloaded: {} Data Points for ({}: {}) from: {}, to {}", resp.observations.len(), resolution, base_data_type, from_chicago_time.to_utc(), to_chicago_time.to_utc()));
 
-                let utc_time = ct_time.to_utc();
+               // Print the response
+               for data in resp.observations {
+                   let time = NaiveDate::parse_from_str(&data.date, "%Y-%m-%d")
+                       .expect("Invalid date format")
+                       .and_hms_opt(7, 30, 0).unwrap();
 
-                let value = f64::from_str(&data.value).unwrap();
+                   let ct_time = Chicago.from_local_datetime(&time)
+                       .earliest().unwrap();
 
-                let values = BTreeMap::new();
-                if !data_map.contains_key(&utc_time) {
-                    let fundamental = Fundamental::new(
-                        symbol.clone(),
-                        utc_time.to_string(),
-                        frequency_to_resolution(resolution_to_frequency(resolution).unwrap()).unwrap(),
-                        values,
-                        None,
-                        None,
-                        fred_data.to_string(),
-                    );
-                    data_map.insert(utc_time, fundamental);
-                }
-                if let Some(fundamental) = data_map.get_mut(&utc_time) {
-                    let value = Decimal::from_f64(value).unwrap();
-                    fundamental.values.insert(units_to_string(unit.clone()), value);
-                    //println!("{:?}", fundamental);
+                   let utc_time = ct_time.to_utc();
+
+                   let value = match f64::from_str(&data.value) {
+                       Ok(value) => value,
+                       Err(_) => continue,
+                   };
+
+                   let values = BTreeMap::new();
+                   if !data_map.contains_key(&utc_time) {
+                       let fundamental = Fundamental::new(
+                           symbol.clone(),
+                           utc_time.to_string(),
+                           frequency_to_resolution(resolution_to_frequency(resolution).unwrap()).unwrap(),
+                           values,
+                           None,
+                           None,
+                           fred_data.to_string(),
+                       );
+                       data_map.insert(utc_time, fundamental);
+                   }
+                   if let Some(fundamental) = data_map.get_mut(&utc_time) {
+                       let value = Decimal::from_f64(value).unwrap();
+                       fundamental.values.insert(units_to_string(unit.clone()), value);
+                       //println!("{:?}", fundamental);
+                   }
+
+                   progress_bar.inc(increment);
+                   last_date = ct_time;
+               }
+           }
+        }
+        if !data_map.is_empty() {
+            let data_points: Vec<BaseDataEnum> = data_map.into_iter().map(|(_, v)| BaseDataEnum::Fundamental(v)).collect();
+            match database.save_data_bulk(data_points, true).await {
+                Ok(_) => {},
+                Err(e) => {
+                    return Err(FundForgeError::ServerErrorDebug(e.to_string()));
                 }
             }
         }
-        for data in data_map {
-            println!("{:?}", data);
-        }
+        progress_bar.finish_and_clear();
+        Ok(())
     }
 }
--- ff_standard_lib/src/product_maps/fred/models.rs
+++ ff_standard_lib/src/product_maps/fred/models.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 use rkyv::{Archive, Deserialize as Deserialize_rkyv, Serialize as Serialize_rkyv};
@@ -6,31 +7,52 @@ use crate::standardized_types::resolution::Resolution;
 #[derive(Clone, Serialize_rkyv, Deserialize_rkyv, Archive, Debug, Copy)]
 #[archive(compare(PartialEq), check_bytes)]
 #[archive_attr(derive(Debug))]
-pub enum FredDataSet {
+pub enum FredDataSetEnum {
     GrossNationalProductPerCapita,
 }
 
-pub fn fred_data_resolutions(data: FredDataSet) -> Vec<Resolution> {
+pub fn fred_data_resolutions(data: FredDataSetEnum) -> Vec<Resolution> {
     match data {
-        FredDataSet::GrossNationalProductPerCapita => vec![Resolution::Year],
+        FredDataSetEnum::GrossNationalProductPerCapita => vec![Resolution::Year],
     }
 }
 
-impl fmt::Display for FredDataSet {
+pub fn fred_release_time_of_day(data: FredDataSetEnum) -> (u8, u8) {
+    match data {
+        _ => (7, 30),
+    }
+}
+
+impl fmt::Display for FredDataSetEnum {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            FredDataSet::GrossNationalProductPerCapita => write!(f, "GNPCA"),
+            FredDataSetEnum::GrossNationalProductPerCapita => write!(f, "GNPCA"),
         }
     }
 }
 
-impl FromStr for FredDataSet {
+impl FromStr for FredDataSetEnum {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "GNPCA" => Ok(FredDataSet::GrossNationalProductPerCapita),
+            "GNPCA" => Ok(FredDataSetEnum::GrossNationalProductPerCapita),
             _ => Err(format!("Unknown dataset: {}", s)),
         }
     }
+}
+
+pub fn get_fed_api_country_format(country_code: &str) -> Option<&'static str> {
+    let mut country_map: HashMap<&str, &str> = HashMap::new();
+
+    // Example country code to full country name mappings
+    country_map.insert("USA", "united states");
+    country_map.insert("CAN", "canada");
+    country_map.insert("DEU", "germany");
+    country_map.insert("GBR", "united kingdom");
+    country_map.insert("FRA", "france");
+    country_map.insert("AUS", "australia");  // Add more as needed
+
+    // Look up the country code in the map
+    country_map.get(country_code).copied()
 }
