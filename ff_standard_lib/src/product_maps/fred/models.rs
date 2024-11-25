de179064e101196a689f075d0bb55ff3a336db27
BurnOutTrader <kevinjamesmonaghan@outlook.com>
2024-11-25 12:17:36 +1100

    work on fred api

--- ff_standard_lib/src/product_maps/fred/models.rs
+++ ff_standard_lib/src/product_maps/fred/models.rs
@@ -8,13 +8,11 @@ use crate::standardized_types::resolution::Resolution;
 #[archive_attr(derive(Debug))]
 pub enum FredDataSet {
     GrossNationalProductPerCapita,
-    GrossDomesticProduct,
 }
 
 pub fn fred_data_resolutions(data: FredDataSet) -> Vec<Resolution> {
     match data {
         FredDataSet::GrossNationalProductPerCapita => vec![Resolution::Year],
-        FredDataSet::GrossDomesticProduct => vec![Resolution::Year],
     }
 }
 
@@ -22,7 +20,6 @@ impl fmt::Display for FredDataSet {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             FredDataSet::GrossNationalProductPerCapita => write!(f, "GNPCA"),
-            FredDataSet::GrossDomesticProduct => write!(f, "GDP"),
         }
     }
 }
@@ -33,7 +30,6 @@ impl FromStr for FredDataSet {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "GNPCA" => Ok(FredDataSet::GrossNationalProductPerCapita),
-            "GDP" => Ok(FredDataSet::GrossDomesticProduct),
             _ => Err(format!("Unknown dataset: {}", s)),
         }
     }
